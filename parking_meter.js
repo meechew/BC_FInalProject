687ef92a978062775f43b724ea1c272295b2e7f4
meechew <cbunt@pdx.edu>
2020-11-28 00:29:14 -0800

    index.html done parking_meter.js started

---
+++ parking_meter.js
@@ -0,0 +1,9 @@
+/*Javascript for Parking Meter DApp*/
+
+/* Check if Metamask is installed. */
+if (typeof window.ethereum !== 'undefined') {
+    console.log('MetaMask is installed!');
+} else {
+    console.log('Please install MetaMask or another browser-based wallet');
+}
+
