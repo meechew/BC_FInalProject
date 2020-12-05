13774abedeec5ba26ee9543c93b4b78c7149ab0f
meechew <cbunt@pdx.edu>
2020-12-04 21:29:20 -0800

    parking_meter.js mostly complete

--- parking_meter.js
+++ parking_meter.js
@@ -9,6 +9,7 @@ if (typeof window.ethereum !== 'undefined') {
 
 const web3 = new Web3(window.ethereum);
 window.ethereum.enable();
+const VOUCHMIN = 1000000000000000;
 
 var ParkingMeterABI = [{"name": "Validated", "inputs": [{"type": "uint256", "name": "ValidTill", "indexed": false}, {"type": "address", "name": "VoucherFor", "indexed": true}], "anonymous": false, "type": "event"}, {"outputs": [], "inputs": [], "stateMutability": "nonpayable", "type": "constructor"}, {"name": "PurchaseVoucher", "outputs": [], "inputs": [{"type": "string", "name": "name"}, {"type": "string", "name": "email"}, {"type": "string", "name": "plate"}], "stateMutability": "payable", "type": "function", "gas": 251890}, {"name": "Withdraw", "outputs": [], "inputs": [], "stateMutability": "nonpayable", "type": "function", "gas": 36896}, {"stateMutability": "payable", "type": "fallback"}, {"name": "Owner", "outputs": [{"type": "address", "name": ""}], "inputs": [], "stateMutability": "view", "type": "function", "gas": 1241}, {"name": "VoucherDex", "outputs": [{"type": "string", "name": "name"}, {"type": "string", "name": "email"}, {"type": "string", "name": "plate"}, {"type": "uint256", "name": "expires"}], "inputs": [{"type": "address", "name": "arg0"}], "stateMutability": "view", "type": "function", "gas": 21079}];
 
@@ -16,30 +17,51 @@ var ParkingMeter = new web3.eth.Contract(ParkingMeterABI, '0x4446A1f42aC5B25986D
 
 /* Purchase a parking voucher using the fields provided. */
 async function purchease() {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    const account = accounts[0];
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const plate = document.getElementById("PlateNumber").value;
-    const time = document.getElementById("time").value;
+    const Accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const Account = Accounts[0];
+    const Name = document.getElementById("name").value;
+    const Email = document.getElementById("email").value;
+    const Plate = document.getElementById("PlateNumber").value;
+    const Time = document.getElementById("time").value;
 
 
-    val = (time/900) * 1000000000000000;
+    val = (Time/900) * VOUCHMIN;
     const  transactionParameters = {
-        from: account,
+        from: Account,
         gasPrice: 0x3D7F2,
         value: val
     };
-    await ParkingMeter.methods.PurchaseVoucher(name, email, plate).send(transactionParameters);
+    await ParkingMeter.methods.PurchaseVoucher(Name, Email, Plate).send(transactionParameters);
 }
 
 async function LookUpWallet() {
     const ReturnLockUp = document.getElementById("ReturnLookUp");
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    const account = accounts[0];
+    ReturnLockUp.firstChild.remove();
+    const Accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const Account = Accounts[0];
 
-    var Return = await ParkingMeter.methods.VucherDex(account).call();
+    var Returned = await ParkingMeter.methods.VoucherDex(Account).call();
 
+    const Name = document.createTextNode("Name: " + Returned.name);
+    const Email = document.createTextNode("Email: " + Returned.email);
+    const Plate = document.createTextNode("Plate: " + Returned.plate);
+    const Expires_D = new Date(parseInt(Returned.expires) * 1000);
+    const Expires = document.createTextNode("Expires: " + Expires_D.toLocaleTimeString());
+
+    const p = document.createElement("p");
+    p.classList.add("ReturnLookUp");
+    p.appendChild(Name);
+    const Br1 = document.createElement("br");
+    p.appendChild(Br1);
+    p.appendChild(Email);
+    const Br2 = document.createElement("br");
+    p.appendChild(Br2);
+    p.appendChild(Plate);
+    const Br3 = document.createElement("br");
+    p.appendChild(Br3);
+    p.appendChild(Expires);
+
+    ReturnLockUp.appendChild(p);
 }
 
 const PurchaseBtn = document.getElementById('purchase');
