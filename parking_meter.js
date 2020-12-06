cb2a90d361a17b7a462f4fc6599763b1de42eff1
Carl Bunt <cbunt@pdx.edu>
2020-12-06 02:48:19 -0800

    Fixed parking_metter.js

--- parking_meter.js
+++ parking_meter.js
@@ -11,9 +11,9 @@ const web3 = new Web3(window.ethereum);
 window.ethereum.enable();
 const VOUCHMIN = 1000000000000000;
 
-var ParkingMeterABI = [{"name": "Validated", "inputs": [{"type": "uint256", "name": "ValidTill", "indexed": false}, {"type": "address", "name": "VoucherFor", "indexed": true}], "anonymous": false, "type": "event"}, {"outputs": [], "inputs": [], "stateMutability": "nonpayable", "type": "constructor"}, {"name": "PurchaseVoucher", "outputs": [], "inputs": [{"type": "string", "name": "name"}, {"type": "string", "name": "email"}, {"type": "string", "name": "plate"}], "stateMutability": "payable", "type": "function", "gas": 251890}, {"name": "Withdraw", "outputs": [], "inputs": [], "stateMutability": "nonpayable", "type": "function", "gas": 36896}, {"stateMutability": "payable", "type": "fallback"}, {"name": "Owner", "outputs": [{"type": "address", "name": ""}], "inputs": [], "stateMutability": "view", "type": "function", "gas": 1241}, {"name": "VoucherDex", "outputs": [{"type": "string", "name": "name"}, {"type": "string", "name": "email"}, {"type": "string", "name": "plate"}, {"type": "uint256", "name": "expires"}], "inputs": [{"type": "address", "name": "arg0"}], "stateMutability": "view", "type": "function", "gas": 21079}];
+var ParkingMeterABI = [{"name":"Validated","inputs":[{"type":"uint256","name":"ValidTill","indexed":false},{"type":"address","name":"VoucherFor","indexed":true}],"anonymous":false,"type":"event"},{"outputs":[],"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"name":"PurchaseVoucher","outputs":[],"inputs":[{"type":"string","name":"name"},{"type":"string","name":"email"},{"type":"string","name":"plate"}],"stateMutability":"payable","type":"function","gas":510922},{"name":"Withdraw","outputs":[],"inputs":[],"stateMutability":"nonpayable","type":"function","gas":36896},{"stateMutability":"payable","type":"fallback"},{"name":"Owner","outputs":[{"type":"address","name":""}],"inputs":[],"stateMutability":"view","type":"function","gas":1241},{"name":"VoucherByAdd","outputs":[{"type":"string","name":"name"},{"type":"string","name":"email"},{"type":"string","name":"plate"},{"type":"uint256","name":"expires"}],"inputs":[{"type":"address","name":"arg0"}],"stateMutability":"view","type":"function","gas":21079},{"name":"VoucherByPlt","outputs":[{"type":"string","name":"name"},{"type":"string","name":"email"},{"type":"string","name":"plate"},{"type":"uint256","name":"expires"}],"inputs":[{"type":"string","name":"arg0"}],"stateMutability":"view","type":"function","gas":21192}];
 
-var ParkingMeter = new web3.eth.Contract(ParkingMeterABI, '0x4446A1f42aC5B25986D33dBAB3aCE0bda3685c0a');
+var ParkingMeter = new web3.eth.Contract(ParkingMeterABI, '0x4bc3f0F82a1d8Ccc35520c060FbDf250A98767be');
 
 /* Purchase a parking voucher using the fields provided. */
 async function Purchease() {
@@ -28,7 +28,7 @@ async function Purchease() {
     val = (Time/900) * VOUCHMIN;
     const  transactionParameters = {
         from: Account,
-        gasPrice: 0x3D7F2,
+        gasPrice: 0x1D91CA3600,
         value: val
     };
     await ParkingMeter.methods.PurchaseVoucher(Name, Email, Plate).send(transactionParameters);
@@ -41,7 +41,7 @@ async function LookUpWallet() {
     const Accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
     const Account = Accounts[0];
 
-    var Returned = await ParkingMeter.methods.VoucherDex(Account).call();
+    var Returned = await ParkingMeter.methods.VoucherByAdd(Account).call();
 
     const Name = document.createTextNode("Name: " + Returned.name);
     const Email = document.createTextNode("Email: " + Returned.email);
