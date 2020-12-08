02d8f43547185b225d71a312448bd3f8e68f910d
meechew <cbunt@pdx.edu>
2020-12-07 18:39:31 -0800

    Removed duplicate code

--- parking_meter.js
+++ parking_meter.js
@@ -34,55 +34,28 @@ async function Purchease() {
     await ParkingMeter.methods.PurchaseVoucher(Name, Email, Plate).send(transactionParameters);
 }
 
-async function LookUpWallet() {
+async function LookUpVoucher(flag) {
+    let Returned;
     const ReturnLockUp = document.getElementById("ReturnLookUp");
     if (ReturnLockUp.firstChild)
         ReturnLockUp.firstChild.remove();
-    const Accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    const Account = Accounts[0];
 
-    var Returned = await ParkingMeter.methods.VoucherByAdd(Account).call();
-
-    if (Returned.expires == 0) {
-        const p = document.createElement("p");
-        const Failed = document.createTextNode("NO RECORDS FOUND");
-        p.appendChild(Failed);
-        p.classList.add("ReturnLookUp");
-        ReturnLockUp.appendChild(p);
-        return;
+    /*Call for voucher by plate number*/
+    if (flag === 'P') {
+        const PlateLookUp = document.getElementById('PlateLookUp').value.toUpperCase();
+        Returned = await ParkingMeter.methods.VoucherByPlt(PlateLookUp).call();
     }
-    
-    const Name = document.createTextNode("Name: " + Returned.name);
-    const Email = document.createTextNode("Email: " + Returned.email);
-    const Plate = document.createTextNode("Plate: " + Returned.plate);
-    const Expires_D = new Date(parseInt(Returned.expires) * 1000);
-    const Expires = document.createTextNode("Expires: " + Expires_D.toLocaleTimeString());
 
-    const p = document.createElement("p");
-    p.classList.add("ReturnLookUp");
-    p.appendChild(Name);
-    const Br1 = document.createElement("br");
-    p.appendChild(Br1);
-    p.appendChild(Email);
-    const Br2 = document.createElement("br");
-    p.appendChild(Br2);
-    p.appendChild(Plate);
-    const Br3 = document.createElement("br");
-    p.appendChild(Br3);
-    p.appendChild(Expires);
+    /*Call for voucher by wallet address*/
+    if (flag === 'W') {
+        const Accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const Account = Accounts[0];
+        Returned = await ParkingMeter.methods.VoucherByAdd(Account).call();
+    }
 
-    ReturnLockUp.appendChild(p);
-}
+    if (Returned == null) return;
 
-async function LookUpPlate() {
-    const ReturnLockUp = document.getElementById("ReturnLookUp");
-    if (ReturnLockUp.firstChild)
-        ReturnLockUp.firstChild.remove();
-    const PlateLookUp = document.getElementById('PlateLookUp').value.toUpperCase();
-
-    var Returned = await ParkingMeter.methods.VoucherByPlt(PlateLookUp).call();
-
-    if (Returned.expires == 0) {
+    if (0 == Returned.expires) {
         const p = document.createElement("p");
         const Failed = document.createTextNode("NO RECORDS FOUND");
         p.appendChild(Failed);
@@ -121,10 +94,10 @@ PurchaseBtn.addEventListener('click', () => {
 
 const LookUpWalletBtn = document.getElementById('LookUpWalletBtn');
 LookUpWalletBtn.addEventListener('click', () => {
-    LookUpWallet();
+    LookUpVoucher('W');
 });
 
 const LookUpPlateBtn = document.getElementById('LookUpPlateBtn');
 LookUpPlateBtn.addEventListener('click', () => {
-    LookUpPlate();
+    LookUpVoucher('P');
 });
